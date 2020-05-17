e4a5d2fdbfd9bad1cf7429122d7bc060fb615309
Kgasr <34871278+Kgasr@users.noreply.github.com>
2020-05-18 04:56:37 +0530

    Update calculator.java

--- src/main/java/websample/calculator.java
+++ src/main/java/websample/calculator.java
@@ -19,7 +19,7 @@ public class calculator extends HttpServlet {
 		String firstint = request.getParameter("first");
 		String secondint = request.getParameter("second");
 		int sum = Integer.parseInt(firstint) + Integer.parseInt(secondint);
-		out.println("<labal id='result'>\"Sum is \"+ sum\"</label>");
+		out.println("<labal id='result'>Sum is "+ sum"</label>");
 		out.println("<br /><br /><a href='index.jsp'>Go to Input Page</a>");
 		
 	}
