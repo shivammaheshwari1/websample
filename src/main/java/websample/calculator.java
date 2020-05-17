6422bae9ff342bec6bac5ffa9c7d400af0d33fe2
Kgasr <34871278+Kgasr@users.noreply.github.com>
2020-05-18 02:14:26 +0530

    Initial commit

---
+++ src/main/java/websample/calculator.java
@@ -0,0 +1,26 @@
+package websample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class calculator extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String firstint = request.getParameter("first");
+		String secondint = request.getParameter("second");
+		int sum = Integer.parseInt(firstint) + Integer.parseInt(secondint);
+		out.println("sum is " + sum);
+		out.println("<br /><br /><a href='index.jsp'>Go to Input Page</a>");
+		
+	}
+}
---
+++ src/test/java/websample/calculatorTest.java
@@ -0,0 +1,26 @@
+package websample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class calculatorTest {
+	
+	@Test
+	 public void check() throws ServletException, IOException {
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		req.addParameter("first", "2");
+		req.addParameter("second", "2");
+		new websample.calculator().doPost(req, res);
+		System.out.println(res.getContentAsString());
+				//new Project().doPost(req, res);
+	    Assert.assertTrue(res.getContentAsString().contains("sum is 4"));
+		//Assert.assertEquals("yes","yes");
+	}
+}
