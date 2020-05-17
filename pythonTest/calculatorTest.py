064dd27f5e4a3f8085d0ff1e8dd1c5f31251f881
Kgasr <34871278+Kgasr@users.noreply.github.com>
2020-05-18 04:35:01 +0530

    Create calculatorTest.py

---
+++ pythonTest/calculatorTest.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options();
+options.add_argument("--headless");
+first = "9"
+second = "10"
+driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver',options=options)
+#driver = webdriver.Firefox(firefox_options=options);
+driver.get("http://localhost:8082/websample/index.jsp")
+current_url=driver.current_url
+assert "Addition" in driver.title
+elem = driver.find_element_by_name("first")
+elem.send_keys(first)
+elem = driver.find_element_by_name("second")
+elem.send_keys(second)
+elem.send_keys(Keys.RETURN)
+
+# wait for URL to change with 15 seconds timeout
+WebDriverWait(driver, 15).until(EC.url_changes(current_url))
+
+# print new URL
+new_url = driver.current_url
+print(new_url)
+
+driver.close()
