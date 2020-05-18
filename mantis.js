e7bb19cef79e6be0ad2d1006ccb0e3f8bb12b740
NAMAN SINGH <namansingh48@gmail.com>
2020-05-19 04:52:23 +0530

    Create mantis.js

---
+++ mantis.js
@@ -0,0 +1,44 @@
+[naman_singh@ptcntttst001 EMS]$ cat mantis.js
+  //var soap = require('/var/lib/jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/node/lib/node_modules/soap');
+  var url = 'http://192.168.1.50:80/api/soap/mantisconnect.php?wsdl';
+  var user = 'administrator';
+  var password = 'root';
+  var pipelineArgs = process.argv.slice(2);
+  var summary = pipelineArgs[0];
+  summary += ' build No. ';
+  summary += pipelineArgs[1];
+  summary += ' FAILURE';
+
+  var desc = 'GENERAL INFO \nProject : '
+  desc += pipelineArgs[0];
+  desc += '\nBUILD URL : ';
+  desc += pipelineArgs[2];
+  desc += '\nConsole Log URL : ';
+  desc += pipelineArgs[3];
+
+
+  var args = {
+        username: user,
+        password: password,
+        issue: {
+           project: {
+              id: 2
+            },
+           category: 'General',
+           summary: summary,
+           description: desc
+       }
+  };
+
+
+
+  soap.createClient(url, function(err, client) {
+
+      client.mc_issue_add(args, function(err1, result) {
+        if(err1)
+            console.log( err1 );
+        else
+            console.log('Issue successfully created');
+      });
+
+  });
